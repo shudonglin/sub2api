cd05fd689c9f47f5e88de146c6ec07168dd52cf8
shaw <shaw-wei@foxmail.com>
2026-05-20 15:07:07 +0800

    test(api-contract): fix admin/settings expected map missing openai_codex_user_agent

--- backend/internal/server/api_contract_test.go
+++ backend/internal/server/api_contract_test.go
@@ -835,6 +835,7 @@ func TestAPIContracts(t *testing.T) {
 					"payment_visible_method_alipay_enabled": true,
 					"payment_visible_method_wxpay_enabled": false,
 					"openai_advanced_scheduler_enabled": true,
+					"openai_codex_user_agent":           "",
 					"openai_fast_policy_settings": {
 						"rules": []
 					},
@@ -1062,6 +1063,7 @@ func TestAPIContracts(t *testing.T) {
 					"payment_visible_method_alipay_enabled": false,
 					"payment_visible_method_wxpay_enabled": false,
 					"openai_advanced_scheduler_enabled": false,
+					"openai_codex_user_agent":           "",
 					"openai_fast_policy_settings": {
 						"rules": []
 					},
